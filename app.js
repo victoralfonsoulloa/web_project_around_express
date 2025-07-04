737fb304bf7c1fe062d6116ea40eb4447ae910c8
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-07-03 23:30:29 -0500

    Connect API to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 // Import routes
 const usersRouter = require('./routes/users');
