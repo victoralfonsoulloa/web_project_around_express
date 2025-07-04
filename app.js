3fdafc929c04bd736670ecc8b8735c5149701cbd
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-07-04 00:24:25 -0500

    Add temporary user authorization middleware for testing card creation

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
-mongoose.connect('mongodb://localhost:27017/aroundb');
+mongoose.connect("mongodb://localhost:27017/aroundb");
+
+// Temporary authorization middleware
+app.use((req, res, next) => {
+  req.user = {
+    _id: "68675e9ed53e5d12399f4edf",
+  };
+  next();
+});
 
 // Import routes
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
 
 // Use routes
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+app.use("/users", usersRouter);
+app.use("/cards", cardsRouter);
 
 // Middleware for non-existent routes
 app.use((req, res) => {
-  res.status(404).json({ message: 'Requested resource not found' });
+  res.status(404).json({ message: "Requested resource not found" });
 });
 
 // 500 error handler
 app.use((err, req, res) => {
-  res.status(500).json({ message: 'An error has occurred on the server' });
+  res.status(500).json({ message: "An error has occurred on the server" });
 });
 
 app.listen(PORT, () => {
